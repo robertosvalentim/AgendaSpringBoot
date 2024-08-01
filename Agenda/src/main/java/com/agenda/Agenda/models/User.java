d42895e251bbf1e53f16656fdd773546d662fb31
Roberto Valentim <robertosvalentim@hotmail.com>
2024-08-01 18:39:20 -0300

    Create model User

---
+++ Agenda/src/main/java/com/agenda/Agenda/models/User.java
@@ -0,0 +1,142 @@
+package com.agenda.Agenda.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Objects;
+
+
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public interface CreateUser {
+    }
+
+    public interface UpdateUser {
+    }
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @Column(name = "phone", length = 20, nullable = false)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(min = 10, max = 20)
+    private String phone;
+
+    @Column(name = "email", length = 100, nullable = false)
+    @Email
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    private String email;
+
+    @Column(name = "address", length = 100, nullable = true)
+    private String address;
+
+    @Column(name = "city", length = 100, nullable = true)
+    private String city;
+
+    @Column(name = "state", length = 100, nullable = true)
+    private String state;
+
+
+    public User() {
+    }
+
+    public User(Long id, String name, String phone, String email, String address, String city, String state) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+
+
+    
+
+}
