7cb1aec455f533c2d96b478eb3661d3435ccd1b5
Roberto Valentim <robertosvalentim@hotmail.com>
2024-08-07 21:32:06 -0300

    reconfigure cors to accept all requests

--- Agenda/src/main/java/com/agenda/Agenda/configs/WebConfig.java
+++ Agenda/src/main/java/com/agenda/Agenda/configs/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
    
     
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
